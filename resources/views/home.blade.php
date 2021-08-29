058a584644e050efaca3a4565cb20a6ffb7d7941
Delus <jeannelkadelus52@gmail.com>
2021-08-29 23:40:43 +0700

    Update Readme and other

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Hello city</title>
+
+        
+    </head>
+    <body >
+        <h1>
+            Hello from Vietnam!
+        </h1>
+        <p>It's currently {{date('h:i A') }}.</p>
+        <footer>
+            <p>&copyright; {{date('Y')}} &middot; <a href="#">About Us</a></p>
+        </footer>
+    </body>
+</html>
